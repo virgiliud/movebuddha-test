dadcb9b3d967265492c22d8b5ea49719f15280b0
Virgil Diaconu <virgiliuweb@gmail.com>
2025-10-24 10:49:19 +0700

    Format

--- wp-content/plugins/dice-blocks/src/comparison-table/index.js
+++ wp-content/plugins/dice-blocks/src/comparison-table/index.js
@@ -6,49 +6,49 @@ import { BLOCK_CLASSES } from './config';
 import { ComparisonTable } from './shared';
 
 const Edit = () => {
-const blockProps = useBlockProps( {
-className: BLOCK_CLASSES,
-} );
+    const blockProps = useBlockProps({
+        className: BLOCK_CLASSES,
+    });
 
-const [ selectedMovers, setSelectedMovers ] = useState( [] );
+    const [selectedMovers, setSelectedMovers] = useState([]);
 
-const toggleMover = ( id ) => {
-setSelectedMovers( ( previous ) =>
-previous.includes( id )
-? previous.filter( ( moverId ) => moverId !== id )
-: [ ...previous, id ]
-);
-};
+    const toggleMover = (id) => {
+        setSelectedMovers((previous) =>
+            previous.includes(id)
+                ? previous.filter((moverId) => moverId !== id)
+                : [...previous, id]
+        );
+    };
 
-const clearAll = () => {
-setSelectedMovers( [] );
-};
+    const clearAll = () => {
+        setSelectedMovers([]);
+    };
 
-return (
-<ComparisonTable
-blockProps={ blockProps }
-selectedMovers={ selectedMovers }
-onToggleMover={ toggleMover }
-onClearAll={ clearAll }
-isInteractive
-showSelectionTray={ false }
-/>
-);
+    return (
+        <ComparisonTable
+            blockProps={blockProps}
+            selectedMovers={selectedMovers}
+            onToggleMover={toggleMover}
+            onClearAll={clearAll}
+            isInteractive
+            showSelectionTray={false}
+        />
+    );
 };
 
 const Save = () => {
-const blockProps = useBlockProps.save( {
-className: BLOCK_CLASSES,
-} );
+    const blockProps = useBlockProps.save({
+        className: BLOCK_CLASSES,
+    });
 
-blockProps[ 'data-comparison-table' ] = 'true';
+    blockProps['data-comparison-table'] = 'true';
 
-return <ComparisonTable blockProps={ blockProps } showSelectionTray />;
+    return <ComparisonTable blockProps={blockProps} showSelectionTray />;
 };
 
-registerBlockType( metadata.name, {
-...metadata,
-edit: Edit,
-save: Save,
-} );
+registerBlockType(metadata.name, {
+    ...metadata,
+    edit: Edit,
+    save: Save,
+});
 
